e78c39eee100a110f86c1fdf3e67e5afc2f5c111
Axel Wegener <11866912+awmath@users.noreply.github.com>
2024-04-29 21:14:40 +0000

    add neovim

--- install.sh
+++ install.sh
@@ -1,11 +1,2 @@
-echo "Add remotes"
-
-for remote in $(env |grep CUSTOM_GIT_REMOTE_); do
-    truncated=${remote#"CUSTOM_GIT_REMOTE_"}
-    remote_name=$(echo $truncated | cut -f1 -d"=" | awk '{print tolower($0)}')
-    remote_url=$(echo $truncated | cut -f2 -d"=")
-    git -C $CODESPACE_VSCODE_FOLDER remote add $remote_name $remote_url
-done
-
-echo "Set noreply email"
-git config user.email github.noreply@sparse-space.de
+# install neovim
+sh install-neovim.sh

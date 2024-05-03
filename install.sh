25e68169ec00a5691ae80d7a462a85aba1f38b77
Axel Wegener <11866912+awmath@users.noreply.github.com>
2024-05-03 21:26:36 +0000

    add tmux

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 # install neovim
 sh install-neovim.sh
 
+# install tooling
+sudo apt update
+sudo apt install tmux
+
 # clone nvim config
 mkdir -p ~/.config/
 git clone https://github.com/awmath/neovim-config ~/.config/nvim

62708d12da4ee89095c17b0a2ae4888ed92ef08c
Axel Wegener <11866912+awmath@users.noreply.github.com>
2024-05-07 13:06:31 +0200

    Update install-neovim.sh
    Update tar with sudo permissions.

--- install-neovim.sh
+++ install-neovim.sh
@@ -1,3 +1,3 @@
-curl -Ls https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz | tar xvfz - -C /opt/
+curl -Ls https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz | sudo tar xvfz - -C /opt/
 sudo ln -s /opt/nvim-linux64/bin/nvim /usr/local/bin
 sudo chmod +x /usr/local/bin/nvim
